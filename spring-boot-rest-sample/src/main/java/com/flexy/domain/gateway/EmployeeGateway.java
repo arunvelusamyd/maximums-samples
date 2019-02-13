186a68773d3355db738035bb92e946cf80f7b822
arunvelusamyd <arunvelusamyd@gmail.com>
2019-02-13 10:02:55 +0800

    Adding example for crnk client

--- spring-boot-rest-sample/src/main/java/com/flexy/domain/gateway/EmployeeGateway.java
+++ spring-boot-rest-sample/src/main/java/com/flexy/domain/gateway/EmployeeGateway.java
@@ -1,11 +1,22 @@
 package com.flexy.domain.gateway;
 
+import com.flexy.domain.dto.Employee;
 import io.crnk.client.CrnkClient;
+import io.crnk.core.queryspec.QuerySpec;
+import io.crnk.core.repository.ResourceRepositoryV2;
+import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+
+@Component
 public class EmployeeGateway {
 
-    public void getEmployee() {
-        CrnkClient crnkClient = new CrnkClient("");
+    public Employee getEmployee(int employeeId) {
+        CrnkClient crnkClient = new CrnkClient("http://localhost:8082");
+        ResourceRepositoryV2<Employee, Serializable> employeeRepo = crnkClient.getRepositoryForType(Employee.class);
+        QuerySpec querySpec = new QuerySpec(Employee.class);
+        Employee employee = employeeRepo.findOne(employeeId, querySpec);
+        return employee;
     }
 
 }
--- spring-boot-rest-sample/src/main/java/com/flexy/domain/model/Student.java
+++ spring-boot-rest-sample/src/main/java/com/flexy/domain/model/Student.java
@@ -32,6 +32,6 @@ public class Student {
     private String levelAndSection;
 
     @JsonProperty("class-teacher")
-    private Object classTeacher;
+    private Teacher classTeacher;
 
 }
