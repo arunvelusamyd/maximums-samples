7ff527ee4055e9a76bf28510131ae23b5ab28b2a
arunvelusamyd <arunvelusamyd@gmail.com>
2020-05-29 01:26:24 +0800

    Added hystrix dashboard add tested

--- spring-boot-cloud-sample/src/main/java/com/flexy/SpringCloudSampleApplication.java
+++ spring-boot-cloud-sample/src/main/java/com/flexy/SpringCloudSampleApplication.java
@@ -3,11 +3,15 @@ package com.flexy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class SpringCloudSampleApplication {
 
     public static void main(String[] args) {
