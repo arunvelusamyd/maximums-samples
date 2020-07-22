fd7e7d70febc4db913994a89f1251e87bbeb36a7
arunvelusamyd <arunvelusamyd@gmail.com>
2020-07-22 17:08:18 +0800

    Crnk upgrade and initial commit of evosuite

--- spring-boot-rest-sample/src/main/java/com/flexy/domain/gateway/EmployeeGateway.java
+++ spring-boot-rest-sample/src/main/java/com/flexy/domain/gateway/EmployeeGateway.java
@@ -2,12 +2,8 @@ package com.flexy.domain.gateway;
 
 import com.flexy.domain.dto.Employee;
 import io.crnk.client.CrnkClient;
-import io.crnk.core.queryspec.QuerySpec;
-import io.crnk.core.repository.ResourceRepositoryV2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.io.Serializable;
 
 @Component
 public class EmployeeGateway {
@@ -17,9 +13,15 @@ public class EmployeeGateway {
 
     public Employee getEmployee(int employeeId) {
         CrnkClient crnkClient = new CrnkClient(employeeBaseUrl);
-        ResourceRepositoryV2<Employee, Serializable> employeeRepo = crnkClient.getRepositoryForType(Employee.class);
+        /*ResourceRepositoryV2<Employee, Serializable> employeeRepo = crnkClient.getRepositoryForType(Employee.class);
         QuerySpec querySpec = new QuerySpec(Employee.class);
-        Employee employee = employeeRepo.findOne(employeeId, querySpec);
+        Employee employee = employeeRepo.findOne(employeeId, querySpec);*/
+        Employee employee = new Employee();
+        employee.setEmployeeId(1001);
+        employee.setEmployeeName("Arun");
+        employee.setEducationQualification("B.E");
+        employee.setGender("M");
+        employee.setRole("Engineer");
         return employee;
     }
 
